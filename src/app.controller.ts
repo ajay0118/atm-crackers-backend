bf08d0d45bfe592afe3872b9b39a5b2f50bc31c3
ajay0118 <psajayvenkatesh0104@gmail.com>
2026-08-09 14:53:48 +0530

    configure environment variables

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly configService: ConfigService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const port = this.configService.get<string>('PORT');
+
+    return `ATM Crackers Backend running on port ${port}`;
   }
 }
